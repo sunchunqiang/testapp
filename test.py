7e69de4edbbacf61121251fa71503be302c40224
Chunqiang SUN <sdechunq@amazon.com>
2018-11-01 22:03:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from subprocess import Popen, PIPE
 import os
 import sys
-import time.sleep
+import time
  
 print("testing {0}...".format(sys.argv[1]));
 for x in range(20):
   print("n = {0}".format(x))
-  sleep(60)
+  time.sleep(60)
