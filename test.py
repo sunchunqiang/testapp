f37e893b6acc847c3e8c1d7b197b204461d415d1
Chunqiang SUN <sdechunq@amazon.com>
2018-11-01 21:42:08 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 import sys
 import time.sleep
  
-print("testing ...");
+print("testing {0}...".format(sys.argv[1]));
 for x in range(20):
-  print("n = {0}", x)
+  print("n = {0}".format(x))
   sleep(60)
